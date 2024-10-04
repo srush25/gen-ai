bb66deae3f91d5bb8d811a79a7e32cd8434756e0
SRUSHTI PAWAR <142104311+srush25@users.noreply.github.com>
2024-10-04 20:55:20 +0530

    Vlsi chatbot mini project

--- app.py
+++ app.py
@@ -17,13 +17,12 @@ genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 
 def get_pdf_text(pdf_docs):
-    text = ""
+    text=""
     for pdf in pdf_docs:
-        pdf_reader = PdfReader(pdf)
+        pdf_reader= PdfReader(pdf)
         for page in pdf_reader.pages:
-            text += page.extract_text()
-    return text
-
+            text+= page.extract_text()
+    return  text
 
 def get_text_chunks(text):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
@@ -32,13 +31,15 @@ def get_text_chunks(text):
     return chunks
 
 
+
 def get_vector_store(text_chunks):
-    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
     vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
     vector_store.save_local("faiss_index")
 
 
 def get_conversational_chain():
+
     prompt_template = """
     Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
     provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
@@ -49,29 +50,32 @@ def get_conversational_chain():
     """
 
     model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3)
-    prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+    prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
     chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
 
     return chain
 
 
 def user_input(user_question):
-    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
-
-    new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
+    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+    
+    new_db = FAISS.load_local("faiss_index", embeddings,allow_dangerous_deserialization=True)
     docs = new_db.similarity_search(user_question)
 
     chain = get_conversational_chain()
 
-    response = chain({"input_documents": docs, "question": user_question}, return_only_outputs=True)
+    
+    response = chain(
+        {"input_documents":docs, "question": user_question}
+        , return_only_outputs=True)
 
-    print(f"Response: {response}")
+    print(response)
     st.write("Reply: ", response["output_text"])
 
 
 def main():
-    st.set_page_config(page_title="Chat PDF")
-    st.header("Chat with PDF using Gemini💁")
+    st.set_page_config("Chat PDF")
+    st.title("Artificial Intelligence :blue[Guide for VLSI]")
 
     user_question = st.text_input("Ask a Question from the PDF Files")
 
@@ -88,6 +92,7 @@ def main():
                 get_vector_store(text_chunks)
                 st.success("Done")
 
-
 if __name__ == "__main__":
     main()
+
+   
